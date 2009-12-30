ae8c5a011ad7b3ee8b67f03c209e8d6455df491b
alex <alex@brick.(none)>
2009-12-30 01:03:18 +0000

    pkg_debunk.rb commited

---
+++ pkg_debunk.rb
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+
+# pkg_debunk.rb used to verify installed packages by FreeBSD ports
+# 
+# by Aleksandar Simic 30 December 2009
+
+
+if ARGV.size < 1
+  puts "supply a name of a installed port"
+  exit
+end
+
+# puts "package: #{ARGV[0]}"
+
+def debunk(arg) 
+  db="/var/db/pkg/"
+
+  # puts "from the method, db: #{db}, prefix: #{$prefix}, arg: #{arg}"
+
+  if File.directory?([db, arg].join)
+
+    pkg = ([db, arg].join) << "/+CONTENTS"
+
+    if File.exists?(pkg)
+      contents = File.new(pkg, "r").collect
+      #puts "grep: #{contents[3].grep(/@cwd/)}"
+      prefix = contents[3].grep(/@cwd\ /).to_s.gsub(/\@cwd\ /, "").chomp
+      missing = []
+
+      contents.each { |line|
+        if not line.to_s =~ /^[@\+]/
+          path = [prefix, line.chomp].join "/"
+          if not File.exists?(path)
+            # puts "can't find .#{path}."
+            missing << path
+          end
+        end
+      }
+
+      if not missing.empty?
+        puts "package: #{arg}"
+        missing.each {|m|
+          puts "  not found: #{m}"
+        }
+      end
+
+    else
+      puts
+      puts "the +CONTENTS file of #{arg} can't be read, exiting ..."
+      puts
+      exit 1
+    end
+
+  else
+    puts
+    puts "can't find #{arg} package, exiting ..."
+    puts
+    exit 1
+  end
+end # def                          
+
+debunk(ARGV[0])
